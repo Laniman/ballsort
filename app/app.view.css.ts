7e554ce9b0fd62bfd665fb4f8ee5a3cb123c280e
George Achkasov <georgij-net@yandex.ru>
2023-08-29 06:39:46 +0400

    replace link and button for $mol_

--- app/app.view.css.ts
+++ app/app.view.css.ts
@@ -1,5 +1,36 @@
 namespace $.$$ {
-	
+
+	$mol_style_define( $mol_link, {
+		color: 'lightgray',
+		padding: [ '0.25rem', '1rem' ]
+	} )
+
+	$mol_style_define( $mol_button, {
+		width: 'fit-content',
+		backgroundColor: 'white',
+		color: 'black',
+		padding: [ '0.6rem', '1rem' ],
+		fontSize: '1.3rem',
+		margin: [ 0, '0.2rem' ],
+		border: {
+			width: '2px',
+			style: 'solid',
+			color: 'lightgray',
+		},
+		cursor: 'pointer',
+		position: 'relative',
+
+		':hover': {
+			backgroundColor: '#f1f1f1',
+		},
+
+		':focus': {
+			outline: 'none',
+			boxShadow: '0 0 0 4px lightblue',
+			borderColor: 'lightblue',
+		},
+	} )
+
 	$mol_style_define( $hype_ballsort_app, {
 		fontFamily: 'system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, BlinkMacSystemFont, Helvetica, Arial, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol',
 		color: '#e1e1e1',
@@ -70,9 +101,9 @@ namespace $.$$ {
 			left: 0,
 			right: 0,
 			background: {
-				color: $mol_style_func.rgba(255, 255, 255, 0.6),
+				color: $mol_style_func.rgba( 255, 255, 255, 0.6 ),
 			},
-			backdropFilter: $mol_style_func.blur(new $mol_style_unit(6, 'px')),
+			backdropFilter: $mol_style_func.blur( new $mol_style_unit( 6, 'px' ) ),
 			alignItems: 'center',
 			paddingTop: '5rem',
 		},
--- link/link.view.css.ts
+++
@@ -1,8 +0,0 @@
-namespace $.$$ {
-
-	$mol_style_define( $hype_ballsort_link, {
-		color: 'lightgray',
-		padding: [ '0.25rem', '1rem' ]
-	} )
-
-}
