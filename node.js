c641fc0863fe1c8a41749177be6d37c485841f63
Laniman <Laniman@users.noreply.github.com>
2023-10-10 07:04:04 +0000

    Deploying to gh-pages from @ Laniman/ballsort@2b8e88f4afaf2fb4e795811a6f5983a316d21c6e 🚀

--- node.js
+++ node.js
@@ -3543,6 +3543,15 @@ var $;
         dom_name() {
             return "a";
         }
+        uri_off() {
+            return "";
+        }
+        uri_native() {
+            return null;
+        }
+        external() {
+            return false;
+        }
         attr() {
             return {
                 ...super.attr(),
