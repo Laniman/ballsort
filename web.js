49aa974248af0bbd4d5c71656eeb5e90efd42b91
Laniman <Laniman@users.noreply.github.com>
2025-09-05 07:09:26 +0000

    Deploying to gh-pages from @ Laniman/ballsort@8e48ec718d27d7074e02f54bd9364ef9aeb8deff 🚀

--- web.js
+++ web.js
@@ -3272,7 +3272,7 @@ var $;
             return node;
         }
         auto() {
-            return null;
+            return [];
         }
         render() {
             const node = this.dom_node_actual();
@@ -5019,6 +5019,7 @@ var $;
                         return response;
                     const headers = new Headers(response.headers);
                     headers.set("$mol_offline", "");
+                    headers.set("Origin-Agent-Cluster", "?1");
                     return new Response(response.body, {
                         status: response.status,
                         statusText: response.statusText,
