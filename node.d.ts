06973e0d6084e637c12c6452e87932dad48b4341
Laniman <Laniman@users.noreply.github.com>
2025-09-26 07:09:27 +0000

    Deploying to gh-pages from @ Laniman/ballsort@8e48ec718d27d7074e02f54bd9364ef9aeb8deff 🚀

--- node.d.ts
+++ node.d.ts
@@ -1793,7 +1793,7 @@ declare namespace $.$$ {
 }
 
 declare namespace $ {
-    function $mol_array_shuffle<Item>(array: readonly Item[]): Item[];
+    function $mol_array_shuffle<Item>(array: readonly Item[]): any[];
 }
 
 declare namespace $ {
--- web.d.ts
+++ web.d.ts
@@ -1474,7 +1474,7 @@ declare namespace $.$$ {
 }
 
 declare namespace $ {
-    function $mol_array_shuffle<Item>(array: readonly Item[]): Item[];
+    function $mol_array_shuffle<Item>(array: readonly Item[]): any[];
 }
 
 declare namespace $ {
