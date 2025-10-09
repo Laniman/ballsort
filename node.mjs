80881dc1418da2ca9e077d70410995f1f3ee7efc
Laniman <Laniman@users.noreply.github.com>
2025-10-09 07:09:35 +0000

    Deploying to gh-pages from @ Laniman/ballsort@8e48ec718d27d7074e02f54bd9364ef9aeb8deff 🚀

--- node.mjs
+++ node.mjs
@@ -4444,7 +4444,6 @@ var $;
         }
         dom_node_actual() {
             const node = this.dom_node();
-            $mol_dom_render_styles(node, this.style_size());
             const attr = this.attr();
             const style = this.style();
             $mol_dom_render_attributes(node, attr);
@@ -4543,12 +4542,6 @@ var $;
         attr() {
             return {
                 mol_theme: this.theme() ?? undefined,
-            };
-        }
-        style_size() {
-            return {
-                minHeight: this.minimal_height(),
-                minWidth: this.minimal_width(),
             };
         }
         style() {
@@ -5344,6 +5337,11 @@ var $;
                     textShadow: '0 0',
                 }
             }
+        },
+        ':empty': {
+            '::after': {
+                content: '"\u00A0"',
+            },
         },
     });
 })($ || ($ = {}));
