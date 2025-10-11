9838af361919ef5a737c8417b68e1edf9f7f25b1
Laniman <Laniman@users.noreply.github.com>
2025-10-11 07:07:28 +0000

    Deploying to gh-pages from @ Laniman/ballsort@8e48ec718d27d7074e02f54bd9364ef9aeb8deff 🚀

--- node.js
+++ node.js
@@ -5309,6 +5309,7 @@ var $;
         boxSizing: 'border-box',
         position: 'relative',
         minWidth: rem(2.5),
+        minHeight: rem(2.5),
         gap: $mol_gap.space,
         border: {
             radius: $mol_gap.round,
@@ -5337,11 +5338,6 @@ var $;
                     textShadow: '0 0',
                 }
             }
-        },
-        ':empty': {
-            '::after': {
-                content: '"\u00A0"',
-            },
         },
     });
 })($ || ($ = {}));
--- node.test.js
+++ node.test.js
@@ -5300,6 +5300,7 @@ var $;
         boxSizing: 'border-box',
         position: 'relative',
         minWidth: rem(2.5),
+        minHeight: rem(2.5),
         gap: $mol_gap.space,
         border: {
             radius: $mol_gap.round,
@@ -5328,11 +5329,6 @@ var $;
                     textShadow: '0 0',
                 }
             }
-        },
-        ':empty': {
-            '::after': {
-                content: '"\u00A0"',
-            },
         },
     });
 })($ || ($ = {}));
