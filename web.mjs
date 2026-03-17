f3a9836c844ad694a1e04144a7b5e9b52bd17471
Laniman <Laniman@users.noreply.github.com>
2026-03-17 07:45:08 +0000

    Deploying to gh-pages from @ Laniman/ballsort@8e48ec718d27d7074e02f54bd9364ef9aeb8deff 🚀

--- web.mjs
+++ web.mjs
@@ -4062,6 +4062,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
