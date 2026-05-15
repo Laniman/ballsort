7abbdbd4b5b52521c8b381309a14aa39e43e2a0d
Laniman <Laniman@users.noreply.github.com>
2026-05-15 09:36:10 +0000

    Deploying to gh-pages from @ Laniman/ballsort@8e48ec718d27d7074e02f54bd9364ef9aeb8deff 🚀

--- web.mjs
+++ web.mjs
@@ -1091,6 +1091,12 @@ var $;
         // [ Symbol.toPrimitive ]() {
         // 	return this.toString()
         // }
+        match(...args) {
+            return this.toString().match(...args);
+        }
+        split(...args) {
+            return this.toString().split(...args);
+        }
         toString() {
             return this.join('\n');
         }
